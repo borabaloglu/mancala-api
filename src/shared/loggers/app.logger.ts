170d672d749ca4537d4a34bb3ea071e2c0b9acb9
Bora Baloglu <baloglub@gmail.com>
2021-01-24 18:02:48 +0300

    feat(src/shared/loggers/app.logger.ts): implement app logger

---
+++ src/shared/loggers/app.logger.ts
@@ -0,0 +1,18 @@
+import dayjs from 'dayjs';
+import pino from 'pino';
+import logger from 'pino-http';
+
+import envConfig from '../configs/env.config';
+
+export default logger(
+  {
+    level: envConfig.isProduction ? 'error' : 'info',
+    formatters: {
+      level: (label: string) => ({ level: label }),
+    },
+    prettyPrint: envConfig.isProduction ? false : { translateTime: true, levelFirst: true },
+  },
+  envConfig.isProduction
+    ? pino.destination(`logs/${dayjs().format('YYYY-MM-DD-HH:mm:ss')}_${process.pid}.log`)
+    : null,
+);
