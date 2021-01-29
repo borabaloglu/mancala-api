044f634da11a85f882bc2a69728a0c04faab746d
Bora Baloglu <baloglub@gmail.com>
2021-01-29 16:44:41 +0300

    feat(src/shared/configs/game.config.ts): implement game configurations

---
+++ src/shared/configs/game.config.ts
@@ -0,0 +1,8 @@
+const NUMBER_OF_PITS = 7;
+const NUMBER_OF_STONES_PER_PIT = 4;
+
+export default {
+  numberOfPits: NUMBER_OF_PITS,
+  mancalaIndex: NUMBER_OF_PITS - 1,
+  numberOfStonesPerPit: NUMBER_OF_STONES_PER_PIT,
+};
