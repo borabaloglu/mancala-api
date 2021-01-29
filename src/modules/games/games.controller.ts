f35bebe1cfdaa27023b3936fa64f6bc697c373bf
Bora Baloglu <baloglub@gmail.com>
2021-01-29 16:53:56 +0300

    feat(src/modules/games/games.controller.ts): implement games controller

---
+++ src/modules/games/games.controller.ts
@@ -0,0 +1,71 @@
+import * as validator from 'class-validator';
+
+import _ from 'lodash';
+
+import { Request, Response, Router } from 'express';
+
+import playerAuth from '../../shared/middlewares/player-auth.middleware';
+import safeThrow from '../../shared/middlewares/safe-throw.middleware';
+import validationMiddleware from '../../shared/middlewares/validation.middleware';
+
+import { CreateGameDto } from './dto/create-game.dto';
+import { JoinGameDto } from './dto/join-game.dto';
+import { MakeMoveDto } from './dto/make-move.dto';
+
+import gamesService from './games.service';
+import gamesHelper from './games.helper';
+
+const router = Router();
+
+/**
+ * (POST) / -> Create new game
+ */
+router.post(
+  '/',
+  validationMiddleware(CreateGameDto),
+  safeThrow(async (req: Request, res: Response) => {
+    const result = await gamesService.create(req.body);
+    res.send(result);
+  }),
+);
+
+/**
+ * (POST) /:gamePin/join -> Join game
+ */
+router.post(
+  '/:gamePin/join',
+  validationMiddleware(JoinGameDto),
+  safeThrow(async (req: Request, res: Response) => {
+    req.body.gamePin = req.params.gamePin;
+
+    const result = await gamesService.join(req.body);
+    res.send(result);
+  }),
+);
+
+/**
+ * (PATCH) /:gamePin/make-move -> Make move if player's turn
+ */
+router.patch(
+  '/:gamePin/make-move',
+  validationMiddleware(MakeMoveDto),
+  playerAuth(),
+  safeThrow(async (req: Request, res: Response) => {
+    const updatedGame = await gamesService.makeMove(req.body, req.body.game);
+
+    if (validator.isDefined(updatedGame.finishedAt)) {
+      const winnerPin = gamesHelper.findWinner(updatedGame);
+      res.send({
+        game: updatedGame,
+        winnerPin,
+      });
+    } else {
+      res.send({
+        board: _.values(updatedGame.board),
+        wonAnotherMove: req.body.game.turnPlayerPin === req.body.playerPin,
+      });
+    }
+  }),
+);
+
+export default router;
