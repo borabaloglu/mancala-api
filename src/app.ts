bd0d191f24b34050789bf411afc3558b3fc1f690
Bora Baloglu <baloglub@gmail.com>
2021-01-29 16:54:29 +0300

    feat(src/app.ts): register games controller to express application

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import express from 'express';
 
 dotenv.config({
   path: process.env.NODE_ENV === 'production' ? '.env' : '.dev.env',
 });
+
+import express from 'express';
+
+import gamesController from './modules/games/games.controller';
 
 import globalExceptionFilter from './shared/exception-filters/global.exception-filter';
 import notFoundExceptionFilter from './shared/exception-filters/not-found.exception-filter';
@@ -14,7 +17,14 @@ const app = express();
 
 app.use(appLogger);
 
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 app.use(bodyParser.json());
+
+app.use('/games', gamesController);
 
 app.use(notFoundExceptionFilter);
 app.use(globalExceptionFilter);
